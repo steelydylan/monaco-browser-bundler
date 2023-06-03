e1989a10ebb6a581f8e830fc147c16e29e012451
steelydylan <ess_president@me.com>
2023-06-03 23:04:36 +0900

    fix

--- src/utils/monaco-types-resolver.tsx
+++ src/utils/monaco-types-resolver.tsx
@@ -1,4 +1,4 @@
-import { languages } from "monaco-editor"
+import { languages } from "monaco-editor";
 
 export const setCompilerOptions = (lang: typeof languages) => {
   lang.typescript.typescriptDefaults.setCompilerOptions({
@@ -12,20 +12,24 @@ export const setCompilerOptions = (lang: typeof languages) => {
     esModuleInterop: true,
     typeRoots: ["node_modules/@types"],
     lib: ["es2016", "dom"],
- });
- lang.typescript.typescriptDefaults.setDiagnosticsOptions({
-  noSemanticValidation: false,
-  noSyntaxValidation: false
-})
+  });
+  lang.typescript.typescriptDefaults.setDiagnosticsOptions({
+    noSemanticValidation: false,
+    noSyntaxValidation: false,
+  });
 };
 
-export const resolveModuleType = async (module: string, lang: typeof languages) => {
-  const res = await fetch(`https://esm.sh/${module}`)
-  const dtsUrl = res.headers.get("x-typescript-types")
-  if (!dtsUrl) return "";
+export const resolveModuleType = async (
+  module: string,
+  lang: typeof languages
+): Promise<void> => {
+  const res = await fetch(`https://esm.sh/${module}`);
+  const dtsUrl = res.headers.get("x-typescript-types");
+  if (!dtsUrl) return;
   const dts = await fetch(dtsUrl);
   const dtsText = await dts.text();
-  lang.typescript.typescriptDefaults.addExtraLib(dtsText, `file:///node_modules/@types/${module}/index.d.ts`);
-}
-
-  
+  lang.typescript.typescriptDefaults.addExtraLib(
+    dtsText,
+    `file:///node_modules/@types/${module}/index.d.ts`
+  );
+};
